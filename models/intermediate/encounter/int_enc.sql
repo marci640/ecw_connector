b4c24613ef6d819fcc7f3cb4b85de2231b070096
marci640 <marcifey@gmail.com>
2025-10-06 16:23:06 -0700

    TODO comments, customize w/args?

--- models/intermediate/encounter/int_enc.sql
+++ models/intermediate/encounter/int_enc.sql
@@ -2,7 +2,7 @@
         cast(encounterID as {{ dbt.type_string() }} ) as encounter_id,
         cast(patientID as {{ dbt.type_string() }} ) as person_id,
         cast(patientID as {{ dbt.type_string() }} ) as patient_id,
-        cast('office visit' as {{ dbt.type_string() }} ) as encounter_type,
+        cast('office visit' as {{ dbt.type_string() }} ) as encounter_type, --TODO: arg for 'telehealth' indentifier (visit type naming convention/coding practice?)
         cast(date as date) as encounter_start_date,
         cast(date as date) as encounter_end_date,
         cast(null as {{ dbt.type_int() }} ) as length_of_stay,
@@ -12,7 +12,7 @@
         cast(null as {{ dbt.type_string() }} ) as admit_type_description,
         cast(null as {{ dbt.type_string() }} ) as discharge_disposition_code,
         cast(null as {{ dbt.type_string() }} ) as discharge_disposition_description,
-        cast(usersProviders.uid as {{ dbt.type_string() }} ) as attending_provider_id,
+        cast(usersProviders.uid as {{ dbt.type_string() }} ) as attending_provider_id, -- TODO: going with resourceID, other clinics may use doctorID - confirm
         cast(usersProviders.uname as {{ dbt.type_string() }} ) as attending_provider_name,
         cast(facility.Id as {{ dbt.type_string() }} ) as facility_id,
         cast(facility.facility_name as {{ dbt.type_string() }} ) as facility_name,
