185f2d7bcb473342cb1e2d5be6ee9b588ba7e5d8
marci640 <marcifey@gmail.com>
2025-11-11 02:37:09 -0800

    location/facility queries

--- models/final/location.sql
+++ models/final/location.sql
@@ -1,16 +1,16 @@
-select {% if target.type == 'fabric' %} top 0 {% else %}{% endif %}
-      cast(null as {{ dbt.type_string() }}) as location_id
-    , cast(null as {{ dbt.type_string() }}) as npi
-    , cast(null as {{ dbt.type_string() }}) as name
-    , cast(null as {{ dbt.type_string() }}) as facility_type
-    , cast(null as {{ dbt.type_string() }}) as parent_organization
-    , cast(null as {{ dbt.type_string() }}) as address
-    , cast(null as {{ dbt.type_string() }}) as city
-    , cast(null as {{ dbt.type_string() }}) as state
-    , cast(null as {{ dbt.type_string() }}) as zip_code
-    , cast(null as {{ dbt.type_float() }}) as latitude
-    , cast(null as {{ dbt.type_float() }}) as longitude
-    , cast(null as {{ dbt.type_string() }}) as data_source
-    , cast(null as {{ dbt.type_string() }}) as file_name
-    , cast(null as {{ dbt.type_timestamp() }}) as ingest_datetime
-{% if target.type == 'fabric' %} {% else %} limit 0 {% endif %}
+select
+      fac.Id as location_id,
+      fac.NPI as npi,
+      fac.name as name,
+      fac.PracticeType as facility_type,
+      fac.PayableTo as parent_organization
+      (COALESCE(fac.AddressLine1,'') || ' ' || COALESCE(fac.AddressLine2,'')) as address,
+      fac.City as city,
+      fac.State as state,
+      fac.Zip as zip_code
+      cast(null as {{ dbt.type_float() }}) as latitude,
+      cast(null as {{ dbt.type_float() }}) as longitude,
+      cast('ecw'  as {{ dbt.type_string() }} ) as data_source,
+      cast(null as {{ dbt.type_string() }} ) as file_name,
+      cast(null as {{ dbt.type_timestamp() }} ) as ingest_datetime
+from {{ ref('stg_ecw__edi_facilities') }} fac
